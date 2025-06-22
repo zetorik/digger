f8861f296da57b0378f836fb5858bcb89e1bad59
Zetorikk <zetorikk@gmail.com>
2025-06-22 17:03:53 +0300

    Init commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <iostream>
+
+int main() {
+    SDL_Window *win = SDL_CreateWindow("Digger", 480, 480, SDL_WINDOW_RESIZABLE);
+
+    return 0;
+}
